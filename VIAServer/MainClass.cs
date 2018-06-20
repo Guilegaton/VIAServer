56d659ad65505b8e688a5cbe139ccbb3153a6d59
Guilegaton <yulii.vedkal@nure.ua>
2018-06-20 23:17:18 +0300

    Добавьте файлы проекта.

---
+++ VIAServer/MainClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIAServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new VIAServer<JSONSerializer<Event>>(8080);
+        }
+    }
+}
